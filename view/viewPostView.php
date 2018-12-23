061af49f9627e5f5cfb6680d57087517dd55be71
FrancisRodier78 <francisrodier78@yahoo.fr>
2018-12-23 14:43:03 +0100

    Begin of branch user

--- view/viewPostView.php
+++ view/viewPostView.php
@@ -21,7 +21,7 @@ if ($_COOKIE['ketto'] == $_SESSION['ketto']) {
             Titre : <input type="text" name="titre" value="<?php echo $post->getTitre(); ?>" /><br />
             Chapo : <input type="text" name="chapo" value="<?php echo $post->getChapo(); ?>" /><br />
             Contenu : <input type="textarea rows="8" cols="60" name="content" value="<?php echo $post->getContent(); ?>" /><br />
-            <input type="hidden" name="idPost" value="<?php echo $id; ?>" />
+            <input type="hidden" name="idPost" value="<?php echo $idPost; ?>" />
             <input type="submit" value="Envoyer le post" name="send post" />
         </p>
     </form>
