a413becf8033de9cfe492e1e45a7b99fb7ab97ed
Rodier <francisrodier78@yahoo.fr>
2020-04-06 17:05:47 +0200

    Modification for design, form.

--- lib/vendors/Model/CommentsManagerPDO.php
+++ lib/vendors/Model/CommentsManagerPDO.php
@@ -9,30 +9,30 @@ class CommentsManagerPDO extends CommentsManager
 {
   protected function add(Comment $comment)
   {
-    $q = $this->dao->prepare('INSERT INTO comments SET user_id = :user_id, new_id = :new_id, content = :content, etat = :etat, dateCreation = NOW()');
- 
-    $q->bindValue(':user_id', $comment->user_id(), \PDO::PARAM_INT);
-    $q->bindValue(':new_id', $comment->new_id(), \PDO::PARAM_INT);
-    $q->bindValue(':content', $comment->content());
-    $q->bindValue(':etat', 'en attente');
- 
-    $q->execute();
+      $requete = $this->dao->prepare('INSERT INTO comments SET user_id = :user_id, new_id = :new_id, content = :content, etat = :etat, dateCreation = NOW()');
+
+      $requete->bindValue(':user_id', $comment->user_id(), \PDO::PARAM_INT);
+      $requete->bindValue(':new_id', $comment->new_id(), \PDO::PARAM_INT);
+      $requete->bindValue(':content', $comment->content());
+      $requete->bindValue(':etat', 'en attente');
+
+      $requete->execute();
  
     $comment->setId($this->dao->lastInsertId());
   }
  
   public function delete($id)
   {
-    $q = $this->dao->prepare('DELETE FROM comments WHERE id = :id');
-    $q->bindValue(':id', (int) $id, \PDO::PARAM_INT);
-    $q->execute();
+      $requete = $this->dao->prepare('DELETE FROM comments WHERE id = :id');
+      $requete->bindValue(':id', (int) $id, \PDO::PARAM_INT);
+      $requete->execute();
   }
  
   public function deleteFromNews($news)
   {
-    $q = $this->dao->prepare('DELETE FROM comments WHERE new_id = :news');
-    $q->bindValue(':news', $news, \PDO::PARAM_INT);
-    $q->execute();
+      $requete = $this->dao->prepare('DELETE FROM comments WHERE new_id = :news');
+      $requete->bindValue(':news', $news, \PDO::PARAM_INT);
+      $requete->execute();
   }
  
   public function getListOf($new_id)
@@ -40,14 +40,14 @@ class CommentsManagerPDO extends CommentsManager
     if (!ctype_digit($new_id)) {
       throw new \InvalidArgumentException('L\'identifiant de la news passé doit être un nombre entier valide');
     }
+
+      $requete = $this->dao->prepare('SELECT id, user_id, new_id, content, etat, dateCreation FROM comments WHERE new_id = :new_id AND etat = \'Validé\'');
+      $requete->bindValue(':new_id', $new_id, \PDO::PARAM_INT);
+      $requete->execute();
+
+      $requete->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\Comment');
  
-    $q = $this->dao->prepare('SELECT id, user_id, new_id, content, etat, dateCreation FROM comments WHERE new_id = :new_id AND etat = \'Validé\'');
-    $q->bindValue(':new_id', $new_id, \PDO::PARAM_INT);
-    $q->execute();
- 
-    $q->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\Comment');
- 
-    $comments = $q->fetchAll();
+    $comments = $requete->fetchAll();
  
     foreach ($comments as $comment)
     {
@@ -59,25 +59,25 @@ class CommentsManagerPDO extends CommentsManager
  
   protected function modify(Comment $comment)
   {
-    $q = $this->dao->prepare('UPDATE comments SET user_id = :user_id, content = :content, etat = :etat WHERE id = :id');
+      $requete = $this->dao->prepare('UPDATE comments SET user_id = :user_id, content = :content, etat = :etat WHERE id = :id');
 
-    $q->bindValue(':user_id', $comment->user_id());
-    $q->bindValue(':content', $comment->content());
-    $q->bindValue(':etat', $comment->etat());
-    $q->bindValue(':id', $comment->id(), \PDO::PARAM_INT);
- 
-    $q->execute();
+      $requete->bindValue(':user_id', $comment->user_id());
+      $requete->bindValue(':content', $comment->content());
+      $requete->bindValue(':etat', $comment->etat());
+      $requete->bindValue(':id', $comment->id(), \PDO::PARAM_INT);
+
+      $requete->execute();
   }
  
   public function get($id)
   {
-    $q = $this->dao->prepare('SELECT id, user_id, new_id, content, etat, dateCreation FROM comments WHERE id = :id');
-    $q->bindValue(':id', (int) $id, \PDO::PARAM_INT);
-    $q->execute();
+      $requete = $this->dao->prepare('SELECT id, user_id, new_id, content, etat, dateCreation FROM comments WHERE id = :id');
+      $requete->bindValue(':id', (int) $id, \PDO::PARAM_INT);
+      $requete->execute();
+
+      $requete->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\Comment');
  
-    $q->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\Comment');
- 
-    return $q->fetch();
+    return $requete->fetch();
   }
 
     public function getListAtt($debut = -1, $limite = -1)
